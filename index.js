f0247c395c85eb6795a2f7ece5a4caab9b7cc890
Esmira11 <ismayilova788@gmail.com>
2023-04-27 05:35:00 +0400

    task completed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-//task 1
+//***OBJECT***
+//TASK 1
 /*
 const users = [
 
@@ -46,7 +47,7 @@ alert(`The following users are online now: ${usersOnlineNames.join()}`)
 */
 
 
-//task 2
+//TASK 2
 /*
 function giveJobToStudent(object,newKey){
     object.jobName = `${newKey}`
@@ -67,7 +68,7 @@ const updatedStudent = giveJobToStudent(student, 'web developer');
 
 
 
-//task 3
+//TASK 3
 /*
 function handleObject(obj,key,action){
    if(action === 'get'){
@@ -95,32 +96,180 @@ const result = handleObject(student, 'programmingLanguage', 'delete');
 console.log('result', result);
 */
 
-// task 4
-
-// function getKiller(suspectInfo,deadPeople){
+// TASK 4
+/*
+function getKiller(suspectInfo,deadPeople){
  
-//         for (suspect in suspectInfo) {
-//             for (dead in deadPeople) {
-//                 suspectInfo[suspect].forEach(element => {
-//                     if(element === deadPeople[dead]){
-//                         console.log(element);
-//                     } 
-//                 });
-//                 // deadPeople[dead] === suspectInfo[suspect]
-//             }
-//         }
+        for (suspect in suspectInfo) {
+            for (dead in deadPeople) {
+                suspectInfo[suspect].forEach(element => {
+                    if(element === deadPeople[dead]){
+                        suspectInfo[suspect].forEach(element => {
+                            if(element === deadPeople[dead+1]){
+                                console.log(suspectInfo.suspect);
+                            } 
+                        })
+                    }
+                })
+            }
+        }
         
-// }
+}
 
-// getKiller(
-//     {
-//         'James': ['Jacob', 'Bill', 'Lucas'],
-//         'Johnny': ['David', 'Kyle', 'Lucas'],
-//         'Peter': ['Lucy', 'Kyle'],
-//     }, ['Lucas', 'Bill']
-// );
+getKiller(
+    {
+        'James': ['Jacob', 'Bill', 'Lucas'],
+        'Johnny': ['David', 'Kyle', 'Lucas'],
+        'Peter': ['Lucy', 'Kyle'],
+    }, ['Lucas', 'Bill']
+);
+*/
+
+//TASK 5
+/* 
+function getRandomNumberInRange(applicants) {
+    const keys = Object.keys(applicants)
+    return keys[Math.floor(Math.random() * keys.length)];
+}
+const todaysWinner = {
+    prize: '$10,000',
+}
+ 
+const winnerApplicants = {
+    '001': {
+        name: 'Max',
+        age: 25
+    },
+    '201': {
+        name: 'Svetlana',
+        age: 20
+    },
+    '304': {
+        name: 'Catherine',
+        age: 35
+    },
+}
+ 
+const resultWinner = getWinner(winnerApplicants, todaysWinner);
+console.log('resultWinner', resultWinner); // { prize: '$10,000', name: 'Maxim', age: 25 }
+
+function getWinner(applicants,winnerObject){
+    let  random = getRandomNumberInRange(applicants);
+    console.log(random)
+    let spread = {
+        ...winnerObject,
+        ...applicants[random],
+    };
+    return spread;
+ }
+*/
+
+//TASK 6
+/*
+function  giveTalonsInOrder(patients,orders){
+    let newArray = [];
+    for (let i=0; i<orders.length;i++){
+        for(let j=0; j<patients.length;j++){
+            if(patients[j].id === orders[i]){
+                newArray[i]=patients[j];
+            }
+        }
+    }
+    return newArray;
+}
+const ordersArr = [4, 2, 1, 3];
+const people = [
+    { id: 1, name: "Maxim" },
+    { id: 2, name: "Nicholas" },
+    { id: 3, name: "Angelina" },
+    { id: 4, name: "Vitaly" },
+];
+const result = giveTalonsInOrder(people, ordersArr);
+for(let i=0;i<result.length;i++){
+    console.log(result[i]);
+}
+*/
+
+//***DATE***
+//TASK 1
+/*
+const date = {
+    day:07,
+    month:11,
+    year:1981
+}
+function  getDateFormat (date,separator){
+        
+        console.log((Object.values(date)).join(separator));
+}
+getDateFormat(date,'.');
+*/
+
+//TASK2
+/*
+let nextBirthdayDate=new Date(2003, 05, 23);
+let today = new Date();
+function getDaysBeforeBirthday(nextBirthdayDate){
+    nextBirthdayDate.setFullYear(today.getFullYear());
+    if (today > nextBirthdayDate) {
+        nextBirthdayDate.setFullYear(today.getFullYear() + 1);
+    }
+    let result =Math.floor((nextBirthdayDate - today) / (1000*60*60*24));
+    console.log(result);
+      
+}
+getDaysBeforeBirthday(nextBirthdayDate);
+*/
 
 
-//task 5
+//Task 3
 
-function getWinner(applicants,winnerObject)
+
+
+//***THIS*** 
+//task 1
+/*
+const student = {
+    stack: ['HTML'],
+    level: 1,
+    improveLevel(n){
+        let best = ["HTML", "CSS", "JavaScript", "React", "NodeJS"];
+        for (let i=1;i<n;i++){
+            student.stack.push(best[i]);
+        }
+            console.log((student.stack).join());
+    }
+}
+student.improveLevel(5);
+*/
+
+//task 2
+// 
+
+//task 3
+/*
+const footballer = {
+    fullName: 'Cristiano Ronaldo',
+    attack(){
+        console.log(`${this.fullName} is now with the ball and starting to attack!`);
+    },
+    scoreGoal(sound) {
+        console.log(`${this.fullName} scored a goal! Wow!`);
+        this.celebrate(sound);
+    },
+    celebrate(sound) {
+        console.log(sound);
+    },
+    goToSubstitution: function(newPlayer) {
+        console.log(`${this.fullName} goes to replace. ${newPlayer} enters the field`);
+    }
+}
+ 
+const attack = footballer.attack;
+const score = footballer.scoreGoal;
+const substitute = footballer.goToSubstitution;
+attack()
+score('Siiiiii');
+substitute('Paulo Dibala')
+*/
+
